45abace505f3fb05f99660c1bca3817e0de715f2
Jonas Ohlsson <jonas.sebastian.ohlsson@gmail.com>
2014-10-05 10:41:38 +0100

    adapted full tests for multi url penthouse

--- test/full-tests.js
+++ test/full-tests.js
@@ -1,96 +1,95 @@
 var penthouse = require('../lib/'),
-    chai = require('chai'),
-    should = chai.should(),
-    css = require('css'),
-    read = require('fs').readFileSync,
-    path = require('path');
+  chai = require('chai'),
+  should = chai.should(),
+  css = require('css'),
+  fs = require('fs'),
+  read = fs.readFileSync,
+  path = require('path'),
+  async = require('async'),
+  glob = require('glob');
+
+//penthouse.DEBUG = true;
 
 describe('penthouse functionality tests', function () {
-    var originalCssFilePath = path.join(__dirname, 'static-server', 'main.css'),
-        originalCss = read(originalCssFilePath).toString(),
-        server, port;
+var page1cssPath = path.join(__dirname, 'static-server', 'page1.css'),
+      sharedCssFilePath = path.join(__dirname, 'static-server', 'shared.css'),
+      originalCss = read(page1cssPath).toString(),
+      page1, server, port;
 
     // phantomjs takes a while to start up
-    this.timeout(2000);
+    this.timeout(5000);
 
     before(function (done) {
-        startServer(function (instance, serverPort) {
-            server = instance;
-            port = serverPort;
-            done();
-        });
+      startServer(function (instance, serverPort) {
+        server = instance;
+        port = serverPort;
+        page1 = ('http://localhost:' + port + '/page1.html');
+        done();
+      });
     });
 
-    after(function () {
-        server.close();
-    });
-	
-    it('should return a css file', function (done) {
-        penthouse({
-            url: 'http://localhost:' + port,
-            css: originalCssFilePath
-        }, function (err, result) {
-            if(err) { done(err); }
-            try {
-                css.parse(result);
-                done();
-            } catch (ex) {
-                done(ex);
-            }
+    after(function (done) {
+      server.close();
+
+      glob("critical-*.css", function(err, files) {
+        if(err) { throw err; }
+
+        async.map(files, fs.unlink, function(err, results){
+          if(err) throw err;
+          done();
         });
+      });
     });
 
-    it('should return a css file whose parsed AST is equal to the the original\'s AST when the viewport is large', function (done) {
-        var widthLargerThanTotalTestCSS = 1000,
-            heightLargerThanTotalTestCSS = 1000;
-        penthouse({
-            url: 'http://localhost:' + port,
-            css: originalCssFilePath,
-            width: widthLargerThanTotalTestCSS,
-            height: heightLargerThanTotalTestCSS
-        }, function (err, result) {
-            try {
-                var resultAst = css.parse(result);
-                var orgAst = css.parse(originalCss);
-                resultAst.should.eql(orgAst);
-                done();
-            } catch (ex) {
-                done(ex);
-            }
-
-        });
+    it('should return the contents of a css file', function (done) {
+      penthouse({
+        urls    : [ page1],
+        cssFile : page1cssPath
+      }, function (err, result) {
+        if (err) {
+          done(err);
+          return;
+        }
+        try {
+          css.parse(result);
+          done();
+        } catch (ex) {
+          done(ex);
+        }
+      });
     });
 
     it('should return a subset of the original AST rules when the viewport is small', function (done) {
-        var widthLargerThanTotalTestCSS = 1000,
-            heightSmallerThanTotalTestCSS = 100;
-        penthouse({
-            url: 'http://localhost:' + port,
-            css: originalCssFilePath,
-            width: widthLargerThanTotalTestCSS,
-            height: heightSmallerThanTotalTestCSS
-        }, function (err, result) {
-            if(err) { done(err); }
-            try {
-                var resultAst = css.parse(result);
-                var orgAst = css.parse(originalCss);
-                resultAst.stylesheet.rules.should.have.length.lessThan(orgAst.stylesheet.rules.length);
-                done();
-            } catch (ex) {
-                done(ex);
-            }
+      var widthLargerThanTotalTestCSS = 1000,
+        heightSmallerThanTotalTestCSS = 100;
+      penthouse({
+        urls    : [page1],
+        cssFile : page1cssPath,
+        width   : widthLargerThanTotalTestCSS,
+        height  : heightSmallerThanTotalTestCSS
+      }, function (err, result) {
+        if (err) { done(err); }
+        try {
+          var resultAst = css.parse(result);
+          var orgAst = css.parse(originalCss);
+          resultAst.stylesheet.rules.should.have.length.lessThan(orgAst.stylesheet.rules.length);
+          // not be empty
+          done();
+        } catch (ex) {
+          done(ex);
+        }
 
-        });
+      });
     });
-	
-	
-	it('@fontface rule should remain, because it is used', function (done) {
+
+
+	it('should keep @fontface rule, because it is used', function (done) {
         var fontFaceRemainCssFilePath = path.join(__dirname, 'static-server', 'fontface--remain.css'),
 			fontFaceRemainCss = read(fontFaceRemainCssFilePath).toString();
-		
+
         penthouse({
-            url: 'http://localhost:' + port,
-            css: fontFaceRemainCssFilePath
+            urls: [page1],
+            cssFile: fontFaceRemainCssFilePath
         }, function (err, result) {
             try {
                 var resultAst = css.parse(result);
@@ -103,14 +102,14 @@ describe('penthouse functionality tests', function () {
 
         });
     });
-	
-	it('@fontface rule should be removed, because it is not used', function (done) {
-        var fontFaceRemoveCssFilePath = path.join(__dirname, 'static-server', 'fontface--remove.css'),
+
+	it('should remove @fontface rule, because it is not used', function (done) {
+      var fontFaceRemoveCssFilePath = path.join(__dirname, 'static-server', 'fontface--remove.css'),
 			fontFaceRemoveCss = read(fontFaceRemoveCssFilePath).toString();
-		
+
         penthouse({
-            url: 'http://localhost:' + port,
-            css: fontFaceRemoveCssFilePath
+            urls: [page1],
+            cssFile: fontFaceRemoveCssFilePath
         }, function (err, result) {
             try {
                 var resultAst = css.parse(result);
@@ -123,14 +122,14 @@ describe('penthouse functionality tests', function () {
 
         });
     });
-	
-	it(':before, :after rules should remain (because el above fold)', function (done) {
-        var pusedoRemainCssFilePath = path.join(__dirname, 'static-server', 'psuedo--remain.css'),
+
+	it('should keep :before, :after rules (because el above fold)', function (done) {
+      var pusedoRemainCssFilePath = path.join(__dirname, 'static-server', 'psuedo--remain.css'),
 			pusedoRemainCss = read(pusedoRemainCssFilePath).toString();
-		
+
         penthouse({
-            url: 'http://localhost:' + port,
-            css: pusedoRemainCssFilePath
+            urls: [page1],
+            cssFile: pusedoRemainCssFilePath
         }, function (err, result) {
             try {
                 var resultAst = css.parse(result);
@@ -143,13 +142,14 @@ describe('penthouse functionality tests', function () {
 
         });
     });
-	
-	it(':hover, :active, etc rules should always be removed', function (done) {
+
+
+	it('should remove :hover, :active, etc rules - always', function (done) {
         var pusedoRemoveCssFilePath = path.join(__dirname, 'static-server', 'psuedo--remove.css');
-		
+
         penthouse({
-            url: 'http://localhost:' + port,
-            css: pusedoRemoveCssFilePath
+            urls: [page1],
+            cssFile: pusedoRemoveCssFilePath
         }, function (err, result) {
             try {
                 result = result.trim();
@@ -161,13 +161,13 @@ describe('penthouse functionality tests', function () {
 
         });
     });
-	
+
 	it('should remove empty rules', function (done) {
         var emptyRemoveCssFilePath = path.join(__dirname, 'static-server', 'empty-rules--remove.css');
-		
+
         penthouse({
-            url: 'http://localhost:' + port,
-            css: emptyRemoveCssFilePath
+            urls: [page1],
+            cssFile: emptyRemoveCssFilePath
         }, function (err, result) {
             try {
 				result = result.trim();
@@ -179,14 +179,14 @@ describe('penthouse functionality tests', function () {
 
         });
     });
-	
+
 	it('should keep @media rules', function (done) {
         var keyframeRemoveCssFilePath = path.join(__dirname, 'static-server', 'media--remain.css'),
 			keyframeRemoveCss = read(keyframeRemoveCssFilePath).toString();
-		
+
         penthouse({
-            url: 'http://localhost:' + port,
-            css: keyframeRemoveCssFilePath
+            urls: [page1],
+            cssFile: keyframeRemoveCssFilePath
         }, function (err, result) {
             try {
                 var resultAst = css.parse(result);
@@ -199,14 +199,14 @@ describe('penthouse functionality tests', function () {
 
         });
     });
-	
+
 	it('should keep self clearing rules when needed to stay outside the fold', function (done) {
         var clearSelfRemainCssFilePath = path.join(__dirname, 'static-server', 'clearSelf--remain.css'),
 			clearSelfRemainCss = read(clearSelfRemainCssFilePath).toString();
-		
+
         penthouse({
-            url: 'http://localhost:' + port + '/clearSelf.html',
-            css: clearSelfRemainCssFilePath
+            urls: ['http://localhost:' + port + '/clearSelf.html'],
+            cssFile: clearSelfRemainCssFilePath
         }, function (err, result) {
             try {
                 var resultAst = css.parse(result);
@@ -219,13 +219,13 @@ describe('penthouse functionality tests', function () {
 
         });
     });
-	
+
 	it('should remove @keyframe rules', function (done) {
         var keyframeRemoveCssFilePath = path.join(__dirname, 'static-server', 'keyframe--remove.css');
-		
+
         penthouse({
-            url: 'http://localhost:' + port,
-            css: keyframeRemoveCssFilePath
+            urls: [page1],
+            cssFile: keyframeRemoveCssFilePath
         }, function (err, result) {
             try {
 				result = result.trim();
@@ -237,7 +237,7 @@ describe('penthouse functionality tests', function () {
 
         });
     });
-	
+
 });
 
 function startServer(done) {
