38e3e277f750470474c84398afeace5c7b597255
Jonas Ohlsson <jonas.sebastian.ohlsson@gmail.com>
2014-10-24 20:31:06 +0100

    improve css preformatter unit test

--- test/full-tests.js
+++ test/full-tests.js
@@ -288,7 +288,19 @@ describe('penthouse functionality tests', function() {
 		try {
 			var resultAst = css.parse(result);
 			var orgAst = css.parse(cssPreformatCss);
+			//with comments stripped out, fewer 'rules' (comments included) in AST
 			resultAst.stylesheet.rules.should.have.length.lessThan(orgAst.stylesheet.rules.length);
+			//but except for comments, (also inside declarations), everything should be the same
+			var orgAstRulesExceptComments = orgAst.stylesheet.rules.filter(function(rule){
+				if(typeof rule.declarations !== "undefined") {
+					rule.declarations = rule.declarations.filter(function(declaration){
+						return declaration.type !== "comment"
+					})
+				}
+				return rule.type !== "comment";
+			})
+			orgAstRulesExceptComments.should.eql(resultAst.stylesheet.rules);
+
 			done();
 		} catch (ex) {
 			done(ex);
