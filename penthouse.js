15116ed13c963d8be6474af7d7b09c15c1702943
Jonas Ohlsson <jonas.sebastian.ohlsson@gmail.com>
2015-02-08 15:54:22 +0000

    0.3.0 release candidate

--- penthouse.js
+++ penthouse.js
@@ -3,7 +3,7 @@ Penthouse CSS Critical Path Generator
 https://github.com/pocketjoso/penthouse
 Author: Jonas Ohlsson
 License: MIT
-Version: 0.2.51
+Version: 0.3.0-rc
 
 USAGE:
     phantomjs penthouse.js [CSS file] [URL to page] > [critical path CSS file]
@@ -26,7 +26,7 @@ parser for the script - can be used both for the standalone node binary and the 
 */
 /*jshint unused:false*/
 
-var usageString = ' [--width <width>] [--height <height>] <url> <main.css>';
+var usageString = '[--width <width>] [--height <height>] <url> <main.css>';
 
 function buildError(msg, problemToken, args) {
     var error = new Error( msg  + problemToken);
