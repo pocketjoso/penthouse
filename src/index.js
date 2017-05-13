268e5b87531af9046194279b2d9456c145e15ffa
Jonas Ohlsson Aden <jonas.sebastian.ohlsson@gmail.com>
2017-05-13 17:27:26 +0200

    add src folder and transpile with babel

---
+++ src/index.js
@@ -0,0 +1,272 @@
+/*
+ * Node module wrapper for the PhantomJS script
+ */
+
+'use strict'
+
+var fs = require('fs')
+var tmp = require('tmp')
+var path = require('path')
+var spawn = require('child_process').spawn
+var phantomjs = require('phantomjs-prebuilt')
+var phantomJsBinPath = phantomjs.path
+var apartment = require('apartment')
+var cssAstFormatter = require('css')
+var osTmpdir = require('os-tmpdir')
+var postformatting = require('./postformatting/')
+var normalizeCss = require('./normalize-css-module')
+
+// for phantomjs
+var configString = '--config=' + path.join(__dirname, 'phantomjs', 'config.json')
+var script = path.join(__dirname, 'phantomjs', 'core.js')
+
+var DEFAULT_VIEWPORT_WIDTH = 1300 // px
+var DEFAULT_VIEWPORT_HEIGHT = 900 // px
+var DEFAULT_TIMEOUT = 30000 // ms
+var DEFAULT_MAX_EMBEDDED_BASE64_LENGTH = 1000 // chars
+var DEFAULT_USER_AGENT = 'Penthouse Critical Path CSS Generator'
+var TMP_DIR = osTmpdir()
+var DEFAULT_RENDER_WAIT_TIMEOUT = 100
+var DEFAULT_BLOCK_JS_REQUESTS = true
+
+var toPhantomJsOptions = function (maybeOptionsHash) {
+  if (typeof maybeOptionsHash !== 'object') {
+    return [];
+  }
+  return Object.keys(maybeOptionsHash).map(function (optName) {
+    return '--' + optName + '=' + maybeOptionsHash[optName];
+  })
+}
+
+function penthouseScriptArgs (options, astFilename) {
+  // need to annotate forceInclude values to allow RegExp to pass through JSON serialization
+  var forceInclude = (options.forceInclude || []).map(function (forceIncludeValue) {
+    if (typeof forceIncludeValue === 'object' && forceIncludeValue.constructor.name === 'RegExp') {
+      return { type: 'RegExp', value: forceIncludeValue.source }
+    }
+    return { value: forceIncludeValue }
+  })
+  // TODO: should just stringify the whole thing and parse inside, rather than doing like this,
+  // since the command line util no longer used
+  return [
+    options.url || '',
+    astFilename,
+    options.width || DEFAULT_VIEWPORT_WIDTH,
+    options.height || DEFAULT_VIEWPORT_HEIGHT,
+    JSON.stringify(forceInclude), // stringify to maintain array
+    options.userAgent || DEFAULT_USER_AGENT,
+    options.renderWaitTime || DEFAULT_RENDER_WAIT_TIMEOUT,
+    (typeof options.blockJSRequests !== 'undefined' ? options.blockJSRequests : DEFAULT_BLOCK_JS_REQUESTS),
+    // object, needs to be stringified
+    JSON.stringify(options.customPageHeaders || {}),
+    m.DEBUG
+  ]
+}
+
+function writeAstToFile (ast) {
+  // save ast to file
+  var tmpobj = tmp.fileSync({dir: TMP_DIR})
+  fs.writeFileSync(tmpobj.name, JSON.stringify(ast))
+  return tmpobj
+}
+
+var m = module.exports = function (options, callback) { // jshint ignore: line
+  var stdOut = ''
+  var stdErr = ''
+  // debugging
+  var START_TIME = Date.now()
+  var debuglog = function (msg, isError) {
+    if (m.DEBUG) {
+      var errMsg = 'time: ' + (Date.now() - START_TIME) + ' | ' + (isError ? 'ERR: ' : '') + msg
+      stdErr += errMsg
+      console.error(errMsg)
+    }
+  }
+  normalizeCss.DEBUG = m.DEBUG
+
+  function generateCriticalCss (ast) {
+    var debuggingHelp = '',
+        cp,
+        killTimeout
+
+    var timeoutWait = options.timeout || DEFAULT_TIMEOUT
+
+    var astTmpobj = writeAstToFile(ast)
+
+    var scriptArgs = penthouseScriptArgs(options, astTmpobj.name)
+
+    var phantomJsArgs = [configString].concat(toPhantomJsOptions(options.phantomJsOptions))
+    phantomJsArgs.push(script)
+    phantomJsArgs = phantomJsArgs.concat(scriptArgs)
+
+    cp = spawn(phantomJsBinPath, phantomJsArgs)
+
+    // Errors arise before the process starts
+    cp.on('error', function (err) {
+      debuggingHelp += 'Error executing penthouse using ' + phantomJsBinPath
+      debuggingHelp += err.stack
+      err.debug = debuggingHelp
+      callback(err)
+      // remove the tmp file we created
+      // library would clean up after process ends, but this is better for long living proccesses
+      astTmpobj.removeCallback()
+    })
+
+    cp.stdout.on('data', function (data) {
+      stdOut += data
+    })
+
+    cp.stderr.on('data', function (data) {
+      stdErr += data
+      debuglog(String(data))
+    })
+
+    cp.on('close', function (code) {
+      if (code !== 0) {
+        debuggingHelp += 'PhantomJS process closed with code ' + code
+      }
+    })
+
+    // kill after timeout
+    killTimeout = setTimeout(function () {
+      var msg = 'Penthouse timed out after ' + timeoutWait / 1000 + 's. '
+      debuggingHelp += msg
+      stdErr += msg
+      cp.kill('SIGTERM')
+    }, timeoutWait)
+
+    cp.on('exit', function (code) {
+      if (code === 0) {
+        // promise purely for catching errors,
+        // that otherwise exit node
+        new Promise(function (resolve) {
+          var finalCss = postformatting(stdOut, {
+            maxEmbeddedBase64Length: typeof options.maxEmbeddedBase64Length === 'number' ? options.maxEmbeddedBase64Length : DEFAULT_MAX_EMBEDDED_BASE64_LENGTH
+          }, m.DEBUG, START_TIME)
+
+          if (finalCss.trim().length === 0) {
+            // TODO: this error should surface to user
+            debuglog('Note: Generated critical css was empty for URL: ' + options.url)
+          } else {
+            // remove irrelevant css properties
+            finalCss = apartment(finalCss, {
+              properties: [
+                '(.*)transition(.*)',
+                'cursor',
+                'pointer-events',
+                '(-webkit-)?tap-highlight-color',
+                '(.*)user-select'
+              ],
+              // TODO: move into core phantomjs script
+              selectors: [
+                '::(-moz-)?selection'
+              ]
+            })
+          }
+          callback(null, finalCss)
+          resolve()
+          return
+        })
+        .catch(function (err) {
+          callback(err)
+        })
+      } else {
+        debuggingHelp += 'PhantomJS process exited with code ' + code
+        var err = new Error(stdErr + stdOut)
+        err.code = code
+        err.debug = debuggingHelp
+        err.stdout = stdOut
+        err.stderr = stdErr
+        callback(err)
+      }
+      // we're done here - clean up
+      clearTimeout(killTimeout)
+      // can't rely on that the parent process will be terminated any time soon,
+      // need to rm listeners and kill child process manually
+      process.removeListener('exit', exitHandler)
+      process.removeListener('SIGTERM', sigtermHandler)
+      // remove the tmp file we created
+      // library would clean up after process ends, but this is better for long living proccesses
+      astTmpobj.removeCallback()
+      cp.kill('SIGTERM')
+    })
+
+    function exitHandler () {
+      cp.kill('SIGTERM')
+    }
+    function sigtermHandler () {
+      cp.kill('SIGTERM')
+      process.exit(0)
+    }
+    process.on('exit', exitHandler)
+    process.on('SIGTERM', sigtermHandler)
+  }
+
+  function generateAstFromCssFile(cssfilepath) {
+    // read the css and parse the ast
+    // if errors, normalize css and try again
+    // only then pass css to penthouse
+    return new Promise(function (resolve, reject) {
+      var css
+      try {
+        css = fs.readFileSync(cssfilepath, 'utf8')
+      } catch (e) {
+        reject(e.message)
+        return
+      }
+      debuglog('opened css file')
+
+      var ast = cssAstFormatter.parse(css, { silent: true })
+      var parsingErrors = ast.stylesheet.parsingErrors.filter(function (err) {
+        // the forked version of the astParser used fixes these errors itself
+        return err.reason !== 'Extra closing brace'
+      })
+      if (parsingErrors.length === 0) {
+        debuglog('parsed ast (without errors)')
+        resolve(ast)
+        return
+      }
+
+      // had breaking parsing errors
+      // NOTE: only informing about first error, even if there were more than one.
+      var parsingErrorMessage = parsingErrors[0].message
+      if (options.strict === true) {
+        reject(parsingErrorMessage)
+        return
+      }
+
+      debuglog('Failed ast formatting css \'' + parsingErrorMessage + '\': ')
+      normalizeCss(
+        {
+          url: options.url || '',
+          css: options.css || '',
+          userAgent: options.userAgent || DEFAULT_USER_AGENT,
+          timeout: options.timeout,
+          debug: m.DEBUG
+        },
+        function (err, normalizedCss) {
+          debuglog('normalized css: ' + (normalizedCss ? normalizedCss.length : typeof normalizedCss))
+          if (!normalizedCss) {
+            reject('Failed to normalize CSS errors. Run Penthouse with \'strict: true\' option to see these css errors.')
+            return
+          }
+          ast = cssAstFormatter.parse(normalizedCss, { silent: true })
+          debuglog('parsed normalised css into ast')
+          var parsingErrors = ast.stylesheet.parsingErrors.filter(function (err) {
+            // the forked version of the astParser used fixes these errors itself
+            return err.reason !== 'Extra closing brace'
+          })
+          if (parsingErrors.length > 0) {
+            debuglog('..with parsingErrors: ' + parsingErrors[0].reason)
+          }
+          resolve(ast)
+          return
+        }
+      )
+    })
+  }
+
+  generateAstFromCssFile(options.css)
+  .then(generateCriticalCss)
+  .catch(callback)
+}
--- src/phantomjs/non-matching-media-query-remover.js
+++ src/phantomjs/non-matching-media-query-remover.js
@@ -1,49 +1,49 @@
-'use strict';
+'use strict'
 
-var cssMediaQuery = require('css-mediaquery');
+var cssMediaQuery = require('css-mediaquery')
 // only filter out: print, min-width > width and min-height > height
 
-function _isMatchingMediaQuery(rule, matchConfig) {
+function _isMatchingMediaQuery (rule, matchConfig) {
   if (rule.type !== 'media') {
     // ignore (keep) all non media query rules
-    return true;
+    return true
   }
 
-  var mediaAST;
+  var mediaAST
   try {
-    mediaAST = cssMediaQuery.parse(rule.media);
+    mediaAST = cssMediaQuery.parse(rule.media)
   } catch (e) {
     // cant parse, most likely browser cant either
-    return false;
+    return false
   }
   var keep = mediaAST.some(function (mq) {
     if (mq.type === 'print') {
-      return false;
+      return false
     }
     // f.e. @media all {}
     // go for false positives over false negatives,
     // i.e. accept @media randomThing {}
     if (mq.expressions.length === 0) {
-      return true;
+      return true
     }
     return mq.expressions.some(function (expression) {
       if (expression.modifier === 'min') {
-        return cssMediaQuery.match('(min-' + expression.feature + ':' + expression.value + ')', matchConfig);
+        return cssMediaQuery.match('(min-' + expression.feature + ':' + expression.value + ')', matchConfig)
       } else {
-        return true;
+        return true
       }
-    });
-  });
-  return keep;
+    })
+  })
+  return keep
 }
 
-function nonMatchingMediaQueryRemover(rules, width, height) {
-  var matchConfig = { type: 'screen', width: width + 'px', height: height + 'px' };
+function nonMatchingMediaQueryRemover (rules, width, height) {
+  var matchConfig = { type: 'screen', width: width + 'px', height: height + 'px' }
   return rules.filter(function (rule) {
-    return _isMatchingMediaQuery(rule, matchConfig);
-  });
+    return _isMatchingMediaQuery(rule, matchConfig)
+  })
 }
 
 if (typeof module !== 'undefined') {
-  module.exports = nonMatchingMediaQueryRemover;
+  module.exports = nonMatchingMediaQueryRemover
 }
