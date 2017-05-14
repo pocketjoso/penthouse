e48bee56aa51be5361073e65b009888c00b6e24a
Jonas Ohlsson Aden <jonas.sebastian.ohlsson@gmail.com>
2017-05-14 12:25:25 +0200

    test: check that module returns working promise

--- test/node-module-tests.js
+++ test/node-module-tests.js
@@ -1,10 +1,17 @@
 'use strict'
 
+import css from 'css'
+import { readFileSync as read } from 'fs'
 import { describe, it } from 'global-mocha'
 import path from 'path'
 import penthouse from '../lib/'
 import chai from 'chai'
 chai.should() // binds globally on Object
+
+// becasuse dont want to fail tests on white space differences
+function normalisedCssAst (cssString) {
+  return css.parse(css.stringify(css.parse(cssString), { compress: true }))
+}
 
 describe('extra tests for penthouse node module', function () {
   var page1cssPath = path.join(__dirname, 'static-server', 'page1.css')
@@ -12,11 +19,32 @@ describe('extra tests for penthouse node module', function () {
   // phantomjs takes a while to start up
   this.timeout(5000)
 
+  // module handles both callback (legacy), and promise
+  it('module invocation should return promise', function (done) {
+    var page1 = path.join(__dirname, 'static-server', 'page1.html')
+    var originalCss = read(page1cssPath).toString()
+
+    const penthousePromise = penthouse({
+      url: page1,
+      css: page1cssPath
+    })
+    penthousePromise
+    .then(result => {
+      var resultAst = normalisedCssAst(result)
+      var expectedAst = normalisedCssAst(originalCss)
+      resultAst.should.eql(expectedAst)
+      done()
+    })
+    .catch(done)
+  })
+
   it('error should not contain debug info', function (done) {
+    // callback
     penthouse({
       url: 'http://localhost.does.not.exist',
       css: page1cssPath
-    }, function (err) {
+    })
+    .catch(err => {
       if (err) {
         if (/^Error: time: 0/.test(err)) {
           done(err)
@@ -34,7 +62,8 @@ describe('extra tests for penthouse node module', function () {
     penthouse({
       url: 'http://localhost.does.not.exist',
       css: page1cssPath
-    }, function (err) {
+    })
+    .catch(err => {
       if (err) {
         // err should have format like:
         // time: 0 | opened css file
