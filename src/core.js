e1893a5a6d08f9f8d323478e92824cc7d05c1493
Jonas Ohlsson Aden <jonas.sebastian.ohlsson@gmail.com>
2017-08-30 21:20:43 +0200

    document why using setJavaScriptEnabled(false) not working

--- src/core.js
+++ src/core.js
@@ -77,7 +77,8 @@ async function pruneNonCriticalCssLauncher ({
       }
 
       if (blockJSRequests) {
-        // crashes..
+        // currently does not work together with page.evaluate calls:
+        // https://github.com/GoogleChrome/puppeteer/issues/562
         // await page.setJavaScriptEnabled(false)
         await blockJsRequests(page)
         debuglog('blocking js requests')
